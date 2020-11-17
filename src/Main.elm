11baf06b0d3971550c6a12c07e397fc272aaa14d
Flavio Corpa <flavio.corpa@47deg.com>
2020-11-17 14:51:15 +0100

    Try closing the dropdowns in a more elegant way 🎩

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,6 @@
 module Main exposing (main)
 
-import Browser exposing (element)
-import Browser.Events exposing (onMouseDown)
+import Browser exposing (sandbox)
 import Bulma.CDN exposing (stylesheet)
 import Bulma.Columns exposing (column, columnModifiers, columns, columnsModifiers)
 import Bulma.Components exposing (card, cardContent, cardHeader, cardIcon, cardTitle, dropdown, dropdownItem, dropdownMenu, dropdownModifiers, dropdownTrigger, message, messageBody)
@@ -12,7 +11,7 @@ import Bulma.Modifiers exposing (Color(..), Size(..), shadowless)
 import Bulma.Modifiers.Typography exposing (textLeft)
 import Html exposing (Html, div, i, text)
 import Html.Attributes exposing (attribute, class, id)
-import Html.Events exposing (onCheck, onClick)
+import Html.Events exposing (custom, onClick, stopPropagationOn)
 import Json.Decode as Json
 
 
@@ -33,9 +32,9 @@ type Menu
     | Category3
 
 
-init : ( Model, Cmd Msg )
+init : Model
 init =
-    ( Model Nothing [], Cmd.none )
+    Model Nothing []
 
 
 
@@ -44,34 +43,34 @@ init =
 
 type Msg
     = ToggleMenu Menu
-    | UpdateChoice Menu Int Bool
+    | UpdateChoice Menu Int
     | OnClickOutside
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         ToggleMenu menu ->
             case model.open of
                 Nothing ->
-                    ( { model | open = Just menu }, Cmd.none )
+                    { model | open = Just menu }
 
                 Just m ->
                     if m == menu then
-                        ( { model | open = Nothing }, Cmd.none )
+                        { model | open = Nothing }
 
                     else
-                        ( { model | open = Just menu }, Cmd.none )
+                        { model | open = Just menu }
 
-        UpdateChoice menu num checked ->
-            if checked then
-                ( { model | selected = ( menu, num ) :: model.selected }, Cmd.none )
+        UpdateChoice menu num ->
+            if List.any (\( m, n ) -> m == menu && n == num) model.selected then
+                { model | selected = List.filter ((/=) ( menu, num )) model.selected }
 
             else
-                ( { model | selected = List.filter ((/=) ( menu, num )) model.selected }, Cmd.none )
+                { model | selected = ( menu, num ) :: model.selected }
 
         OnClickOutside ->
-            ( { model | open = Nothing }, Cmd.none )
+            { model | open = Nothing }
 
 
 
@@ -87,12 +86,11 @@ dropTrigger menu =
                 , rounded = True
                 , iconRight = Just ( Large, [], i [ class "fas fa-chevron-down" ] [] )
             }
-            [ onClick <| ToggleMenu menu
+            [ stopPropagationOn "click" (Json.map (\m -> ( m, True )) (Json.succeed <| ToggleMenu menu))
             , attribute "aria-haspopup" "true"
             , attribute "aria-controls" "dropdown-menu"
             ]
-            [ text "COMPARE"
-            ]
+            [ text "COMPARE" ]
         ]
 
 
@@ -101,7 +99,9 @@ checkBox menu num =
     controlCheckBox False
         []
         []
-        [ onCheck <| UpdateChoice menu num ]
+        -- TODO: this keeps being propagated even with a "custom" event :(
+        [ stopPropagationOn "input" (Json.map (\m -> ( m, True )) (Json.succeed <| UpdateChoice menu num))
+        ]
         [ text <| "Option " ++ String.fromInt num ]
 
 
@@ -154,7 +154,14 @@ viewCard title shadow { open, selected } menu =
 
 view : Model -> Html Msg
 view model =
-    div []
+    div
+        (case model.open of
+            Nothing ->
+                []
+
+            Just _ ->
+                [ onClick OnClickOutside ]
+        )
         [ stylesheet
         , section NotSpaced
             []
@@ -188,58 +195,9 @@ view model =
 
 
 
----- SUBSCRIPTIONS ----
-
-
-outsideTarget : String -> Json.Decoder Msg
-outsideTarget elemId =
-    Json.field "target" (isOutside elemId)
-        |> Json.andThen
-            (\isOut ->
-                if isOut then
-                    Json.succeed OnClickOutside
-
-                else
-                    Json.fail "inside target element"
-            )
-
-
-isOutside : String -> Json.Decoder Bool
-isOutside elem =
-    Json.oneOf
-        [ Json.field "id" Json.string
-            |> Json.andThen
-                (\id ->
-                    if elem == id then
-                        Json.succeed False
-
-                    else
-                        Json.fail "check parent node"
-                )
-        , Json.lazy (\_ -> isOutside elem |> Json.field "parentNode")
-        , Json.succeed True
-        ]
-
-
-subscriptions : Model -> Sub Msg
-subscriptions { open } =
-    case open of
-        Nothing ->
-            Sub.none
-
-        Just _ ->
-            onMouseDown <| outsideTarget "dropdown"
-
-
-
 ---- PROGRAM ----
 
 
 main : Program () Model Msg
 main =
-    element
-        { view = view
-        , init = always init
-        , update = update
-        , subscriptions = subscriptions
-        }
+    sandbox { view = view, init = init, update = update }
